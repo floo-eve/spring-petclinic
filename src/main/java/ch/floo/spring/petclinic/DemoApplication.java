2ce973a13cbaacb00b19db67e9518e941c68dced
Eveline Meyer <floo@devzone.ch>
2018-12-27 21:53:37 +0100

    initial commit

---
+++ src/main/java/ch/floo/spring/petclinic/DemoApplication.java
@@ -0,0 +1,14 @@
+package ch.floo.spring.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
